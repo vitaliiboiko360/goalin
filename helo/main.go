75d32efc82edbf0dc67f1eac75d6b81956539659
vitalii boiko <vukelan@gmail.com>
2024-12-29 22:16:10 +0200

    add gin

--- helo/main.go
+++ helo/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
-
-import "rsc.io/quote"
+import (
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println(quote.Opt())
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
